1f25363f1540de612092f0946849e5309e4ee859
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-12-23 18:07:58 +0700

    feat: add index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from "next";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <h1>Next Auth App</h1>
+      <p>
+        This is a sample project that uses{" "}
+        <a href={`https://github.com/iaincollins/next-auth`}>NextAuth.js</a> to
+        add authentication to <a href={`https://nextjs.org/`}>Next.js</a>.
+      </p>
+      <p>
+        See <a href={`https://next-auth.js.org/`}>next-auth.js.org</a> for more
+        information and documentation.
+      </p>
+    </Layout>
   );
 };
 
